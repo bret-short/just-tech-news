66034f52e808d5e12aa4299a1bbbd417ba469188
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-02-18 14:19:56 -0700

    add single post features

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,5 @@ const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
 router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
 
 module.exports = router;
